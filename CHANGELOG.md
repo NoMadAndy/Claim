ae2c55c5825afe54f6a9bf8a99ba891112117423
NoMadAndy <31357725+NoMadAndy@users.noreply.github.com>
2025-12-06 23:09:12 +0000

    2025-12-06 23:09:12: app.js, index.html

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 2025-12-06 23:09:12
+**Files:** 2fileschanged,5insertions(+),5deletions(-)
+**Modified:**
+- `frontend/app.js`
+- `frontend/index.html`
+
 ## 2025-12-06 23:09:00
 **Files:** 1filechanged,14insertions(+),9deletions(-)
 **Modified:**
--- frontend/app.js
+++ frontend/app.js
@@ -16,8 +16,8 @@ class SoundManager {
         this.unlocked = false; // NEW: Track if unlock button was pressed
         this.setupGlobalListeners();
         // Version tag for debugging
-        if (window.debugLog) window.debugLog('SoundManager init v1765056300');
-        console.log('SoundManager init v1765056300');
+        if (window.debugLog) window.debugLog('SoundManager init v1765056600');
+        console.log('SoundManager init v1765056600');
         // Do NOT auto-create AudioContext on load (iOS blocks it). Create lazily on first gesture or play.
         // Do NOT setup global listeners - only manual unlock button
     }
